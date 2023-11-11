7b11c6596d3e30c1c937c1ed74edf50e19564987
GreatStack <149926431+wizardofcode8@users.noreply.github.com>
2023-11-11 16:38:33 +0500

    studentanimation.js

---
+++ signupanimation.js
@@ -0,0 +1,12 @@
+gsap.from("#signanimation",{
+    scale: 0.1,
+    opacity:0,
+    duration: 3,
+    delay:0.5
+})
+gsap.from("#head",{
+    scale: 0.5,
+    opacity:0,
+    duration: 3,
+    delay:1.5
+})
