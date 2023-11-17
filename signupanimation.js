8b6acfcf391cda7de3c04bdf1fd3db65a76e79a2
GreatStack <149926431+wizardofcode8@users.noreply.github.com>
2023-11-17 14:59:30 +0500

    animation add

--- signupanimation.js
+++ signupanimation.js
@@ -1,12 +1,5 @@
-gsap.from("#signanimation",{
-    scale: 0.1,
+gsap.from("#col-first, #col-second",{
     opacity:0,
-    duration: 3,
+    duration: 3.5,
     delay:0.5
-})
-gsap.from("#head",{
-    scale: 0.5,
-    opacity:0,
-    duration: 3,
-    delay:1.5
-})
+});
